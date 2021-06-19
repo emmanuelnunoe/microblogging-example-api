4e491cae3b6641927c21ade64f48c1c66b502f79
emmanuelnunoe <39935404+emmanuelnunoe@users.noreply.github.com>
2021-06-18 22:43:08 -0700

    port update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const app = require('./app');
 const server = require('http').Server(app);
 const debug = require('debug')('app:server');
 
-const port = process.env.NODE_PORT || 5000;
+const port = process.env.PORT || 5000;
 server.listen(port, () => {
   debug(`Servidor corriendo en el puerto: ${server.address().port}`);
 });
