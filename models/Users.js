ce33b14ca14348c676d751e85be259a145a85746
Emmanuel <emmanuel.nuno.estrella@uabc.edu.mx>
2021-08-16 18:55:13 -0700

    Update Users.js
    update user model

--- models/Users.js
+++ models/Users.js
@@ -1,10 +1,13 @@
-let mongoose = require('mongoose');
-let Schema = mongoose.Schema;
-let Post = require('../models/Posts.js');
-//para la encriptacion del password
-let bcrypt = require('bcryptjs');
-let SALT_WORK_FACTOR = 10;
-let UserSchema = new Schema({
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+var Post = require('../models/Post.js');
+
+//For password encrypt
+var bcrypt = require('bcryptjs');
+var SALT_WORK_FACTOR = 10;
+
+var UserSchema = new Schema({
   username: { type: String, required: true, index: { unique: true } },
   password: { type: String, required: true },
   fullname: String,
@@ -15,16 +18,16 @@ let UserSchema = new Schema({
 });
 
 UserSchema.pre('save', function (next) {
-  let user = this;
-  // solo aplica una funcion hash al password si ha sido modificado (o es nuevo)
+  var user = this;
+  // only hash the password if it has been modified (or is new)
   if (!user.isModified('password')) return next();
-  // genera la salt
+  // generate a salt
   bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
     if (err) return next(err);
-    // aplica una funcion hash al password usando la nueva salt
+    // hash the password using our new salt
     bcrypt.hash(user.password, salt, function (err, hash) {
       if (err) return next(err);
-      // sobrescribe el password escrito con el hasheado
+      // override the cleartext password with the hashed one
       user.password = hash;
       next();
     });
