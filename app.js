914d2a54f19d1605f866ad3ef4735361da002701
Emmanuel <emmanuel.nuno.estrella@uabc.edu.mx>
2021-08-16 18:12:18 -0700

    update app.js

--- app.js
+++ app.js
@@ -8,31 +8,47 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
 var logger = require('morgan');
-var cors = require('cors');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var postsRouter = require('./routes/posts');
+
+var bodyParser = require('body-parser'); //nuevo
+var cors = require('cors'); //nuevo
+
+require('dotenv').config();
+
 var mongoose = require('mongoose');
+
+// Mongoose conection2
+mongoose.set('useCreateIndex', true);
+mongoose
+  .connect(process.env.DB_URI, { useNewUrlParser: true })
+  .then(() => console.log('mymerndb connection succesful'))
+  .catch((err) => console.error(err));
+
 var app = express();
+
 app.use('/posts', postsRouter);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 // app.use(express.json());
 // app.use(express.urlencoded({ extended: false }));
+app.use(cors()); //nuevo
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb', extended: true }));
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use(cors());
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ extended: true, limit: '50mb', extended: true }));
+app.use('/posts', postsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -49,12 +65,5 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.json({ message: err.message, error: err });
 });
-
-// Mongoose conection2
-mongoose.set('useCreateIndex', true);
-mongoose
-  .connect(process.env.DB_URI, { useNewUrlParser: true })
-  .then(() => console.log('mymerndb connection succesful'))
-  .catch((err) => console.error(err));
 
 module.exports = app;
